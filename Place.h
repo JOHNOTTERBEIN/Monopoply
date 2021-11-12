6d39fdaba099ab6fffaebcd9ccd6939cc287f334
adni8025 <89491754+adni8025@users.noreply.github.com>
2021-11-11 21:13:33 -0700

    Update Place.h

--- Place.h
+++ Place.h
@@ -2,3 +2,48 @@
 // Author: John Otterbein, Adrian Nica
 // Recitation: 215 – Luis Mieses Gomez, Recitation: 118 – Naga Sistla
 // Project 3
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <string>
+
+using namespace std;
+
+class Player {
+
+	private:
+	    string playerName;
+	    char gamePiece;
+	    int totalMoneyAmount;
+	    int location;
+	    bool indexOfLocation[40];	  
+	    bool alive;  
+	    
+	public:
+	    //constructors
+	    Player();
+	    Player(int money);
+
+	    //setters
+	    void setPlayerName(string inputName);
+	    void setOwnedProperty(int inputIndex, bool isOwned); 
+	    void setLocation(int currentPlace); 
+	    void setGamePieceName(string inputPieceName);
+	    void setMoneyAmount(int inputNum); 
+	
+	    //getters
+	    string getPlayerName();
+	    bool getOwnedProperty(int inputIndex);
+	    int getLocation();
+	    bool isAlive();
+	    string getGamePieceName();
+       int getMoneyAmount();
+
+	    //functions 
+       void giveMoney(int giveAmount);
+	    void takeMoney(int takeAmount);
+	    void movePlayer(int moveAmount);  
+
+};
+#endif
