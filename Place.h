a96c8e78fee976f77b3bc00c3bfd4bbeead06395
adni8025 <89491754+adni8025@users.noreply.github.com>
2021-11-11 21:07:15 -0700

    Update Place.h

--- Place.h
+++ Place.h
@@ -1,4 +1,4 @@
 // CS1300 Fall 2021
-// Author: John Otterbein
-// Recitation: 215 – Luis Mieses Gomez
+// Author: John Otterbein, Adrian Nica
+// Recitation: 215 – Luis Mieses Gomez, Recitation: 118 – Naga Sistla
 // Project 3
