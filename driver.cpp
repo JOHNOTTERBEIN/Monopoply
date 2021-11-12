d074237cdb7b398410de3081f4ff20733a103784
JOHNOTTERBEIN <89489022+JOHNOTTERBEIN@users.noreply.github.com>
2021-11-11 22:32:46 -0700

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -1,4 +1,33 @@
 // CS1300 Fall 2021
-// Author: John Otterbein
-// Recitation: 215 – Luis Mieses Gomez
+// Author: John Otterbein, Adrian Nica
+// Recitation: 215 – Luis Mieses Gomez, Recitation: 118 – Naga Sistla
 // Project 3
+
+#include <iostream>
+#include <string>
+#include "Card.h"
+#include "Place.h"
+#include "Dice.h"
+#include "Player.h"
+using namespace std;
+
+int main(){
+    Place p1 = Place();
+    Place p2 = Place("Board Walk", 2000, 400, 0);
+    cout << "p1's name is : " << p1.getName() << " their price is: " << p1.getPrice() << ", the rental price is: " << p1.getRent() << ", the numer of houses is: " << p1.getHouses() << endl;
+    cout << "p2's name is : " << p2.getName() << " their price is: " << p2.getPrice() << ", the rental price is: " << p2.getRent() << ", the numer of houses is: " << p2.getHouses() << endl;
+    p1.setName("Pa ave");
+    p1.setPrice(1500);
+    p1.setRent(150);
+    p1.setHouses(2);
+    cout << "p1's name is : " << p1.getName() << " their price is: " << p1.getPrice() << ", the rental price is: " << p1.getRent() << ", the numer of houses is: " << p1.getHouses() << endl;
+    Card c1 = Card("Mystery Card", "Add 200", 0, false, 200, true);
+    cout << "c1's card name is: " << c1.getName() << " their description is: " << c1.getDes() << ", how much movement does the card cause: " << c1.getMove() << ", does it cause a movement? " << c1.getMovetf() << "How much money is in this card : " << c1.getMoney() << ", Does this cause a change in money? " << c1.getMoneytf() << endl;
+    c1.setName("Community Card");
+    c1.setDes("Move 5 spaces forward");
+    c1.setMove(5);
+    c1.setMovetf(true);
+    c1.setMoney(0);
+    c1.setMoneytf(false);
+    cout << "c1's card name is: " << c1.getName() << " their description is: " << c1.getDes() << ", how much movement does the card cause: " << c1.getMove() << ", does it cause a movement? " << c1.getMovetf() << "How much money is in this card : " << c1.getMoney() << ", Does this cause a change in money? " << c1.getMoneytf() << endl;
+}
