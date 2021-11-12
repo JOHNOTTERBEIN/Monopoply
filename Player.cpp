4b7d2cc8b71fb2d9b18a033d8edf7c932b58b98b
adni8025 <89491754+adni8025@users.noreply.github.com>
2021-11-11 21:14:34 -0700

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -2,3 +2,69 @@
 // Author: John Otterbein, Adrian Nica
 // Recitation: 215 – Luis Mieses Gomez, Recitation: 118 – Naga Sistla
 // Project 3
+
+#include "Player.h"
+
+Player::Player(){
+	totalMoneyAmount = 1500;
+	location = 0;
+	for(int i = 0; i < 40; i++)
+    {
+		indexOfLocation[i] = false;
+	}
+	alive = true;
+	playerName = "";
+}
+
+Player::Player(int money){
+	totalMoneyAmount = money;
+	location = 0;
+	for(int i = 0; i < 40; i++){
+		indexOfLocation[i] = false;
+	}
+	alive = true;
+}
+void Player::setPlayerName(string inputName){
+	playerName = inputName;
+}
+
+void Player::setLocation(int currentPlace){
+	location = currentPlace;
+	
+}
+
+string Player::getPlayerName(){
+	return playerName;
+}
+
+void Player::giveMoney(int giveAmount){
+	totalMoneyAmount += giveAmount;
+}
+
+void Player::takeMoney(int takeAmount){
+	totalMoneyAmount -= takeAmount;
+}
+
+int Player::getMoneyAmount(){
+	return totalMoneyAmount;
+}
+
+void Player::movePlayer(int moveAmount){
+	location += moveAmount;
+}
+
+int Player::getLocation(){
+	return location;
+}
+
+void Player::setOwnedProperty(int inputIndex, bool isOwned){
+	indexOfLocation[inputIndex] = isOwned;
+}
+
+bool Player::getOwnedProperty(int inputIndex){
+	return indexOfLocation[inputIndex];
+}
+
+bool Player::isAlive(){
+	return alive;
+}
