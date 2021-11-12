66b669a3f677a9046140a8545d6455f2ff179212
adni8025 <89491754+adni8025@users.noreply.github.com>
2021-11-11 19:50:47 -0700

    Update Dice.h

--- Dice.h
+++ Dice.h
@@ -1,6 +1,6 @@
 // CS1300 Fall 2021
-// Author: John Otterbein
-// Recitation: 215 – Luis Mieses Gomez
+// Author: John Otterbein, Adrian Nica
+// Recitation: 215 – Luis Mieses Gomez, Recitation: 118 – Naga Sistla
 // Project 3
 
 #ifndef DICE_H
@@ -9,16 +9,16 @@
 class Dice 
 {
 	private:
-	    int numSides;
-		int playerRoll;
+	int numSides;
+	int playerRoll;
         
 	public:
-	    //constructor
-	    Dice(); 
-		//setters
-		void setRollDice(int);
-	    //getters
-	    int getRollDice();	    
+	//constructor
+	Dice(); 
+	//setters
+	void setRollDice(int);
+	//getters
+	int getRollDice();	    
       
         
 };
