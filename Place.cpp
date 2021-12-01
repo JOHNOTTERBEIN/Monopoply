43b8444ebdf87335e030e1e067d3552407fccb4f
JOHNOTTERBEIN <89489022+JOHNOTTERBEIN@users.noreply.github.com>
2021-12-01 16:57:35 -0700

    Update Place.cpp

--- Place.cpp
+++ Place.cpp
@@ -13,13 +13,21 @@ Place::Place(){
     name = "";
     price = 0;
     rent = 0;
-    houses = 0;
+    numHouses = 0;
+    house1 = 0;
+    house2 = 0;
+    house3 = 0;
+    house4 = 0;
 }
-Place::Place(string n, int p, int r, int h){
+Place::Place(string n, int p, int r, int nh, int h1, int h2, int h3, int h4){
     name = n;
     price = p;
     rent = r;
-    houses = h;
+    numHouses = nh;
+    house1 = h1;
+    house2 = h2;
+    house3 = h3;
+    house4 = h4;
 }
 
 //getters
@@ -33,7 +41,7 @@ int Place::getRent(){
     return rent;
 }
 int Place::getHouses(){
-    return houses;
+    return numHouses;
 }
 
 //setters
@@ -46,12 +54,40 @@ void Place::setPrice(int p){
 void Place::setRent(int r){
     rent = r;
 }
-void Place::setHouses(int h){
-    houses = h;
+void Place::setHouses(int h, int num){
+    if(num==1){
+        house1 = h;
+    }
+    else if(num==2){
+        house2 = h;
+    }
+    else if(num==3){
+        house3 = h;
+    }
+    else if(num==4){
+        house4 = h;
+    }
 }
 
 //functions
 
 //Adds a house to the place and increases the rent in accordance
 //Makes sure 5 is the max amount of houses
-//int Place::addHouse(int num);
+int Place::addHouse(int num){
+    if(numHouses == 0){
+        numHouses++;
+        setRent(house1);
+    }
+    else if(numHouses == 1){
+        numHouses++;
+        setRent(house2);
+    }
+    else if(numHouses == 2){
+        numHouses++;
+        setRent(house3);
+    }
+    else{
+        numHouses++;
+        setRent(house4);
+    }
+}
