ef80575fd219e4ba196a58fa3439aebf4ee1c6f5
JOHNOTTERBEIN <89489022+JOHNOTTERBEIN@users.noreply.github.com>
2021-12-02 18:54:50 -0700

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -4,19 +4,20 @@ int num = 0;
                 cin>>num;
                 switch(num){
                     case 1:
+                    {
                         vector<Place> p = pl1.listPlaces();
                         string names;
                         for(int i = 0; i<p.size(); i++){
-                            if(p[i].getHouses!=0){
+                            if(p[i].getHouses()!=0){
                                 names += p[i].getName() + ", ";
                             }
                         }
                         if(names == ""){
-                            cout << "You currently have no houses to sell." << endl;
+                            cout << pl1.getPlayerName() << " currently has no houses to sell." << endl;
                         }
                         else{
                             string name1;
-                            cout << "You currently have places, " << names << "with houses on them. Which would you like to sell?" << endl;
+                            cout << pl1.getPlayerName() << " currently has places, " << names << "with houses on them. Which would you like to sell?" << endl;
                             cin >> name1;
                             for(int i = 0; i<p.size(); i++){
                                 if(p[i].getName()==name1){
@@ -31,34 +32,36 @@ int num = 0;
                             }
                         }
                         break;
+                    }   
                     case 2:
-                        vector<Place> p = pl1.listPlaces();
-                        string names;
-                        for(int i = 0; i<p.size(); i++){
-                            names += p[i].getName() + ", ";
+                    {
+                        vector<Place> p1 = pl1.listPlaces();
+                        string names2;
+                        for(int i = 0; i<p1.size(); i++){
+                            names2 += p1[i].getName() + ", ";
                         }
-                        if(names == ""){
-                            cout << "You currently have no properties to sell." << endl;
+                        if(names2 == ""){
+                            cout << pl1.getPlayerName() << " currently has no properties to sell." << endl;
                         }
                         else{
-                            string name1;
-                            cout << "You currently have places, " << names << "which would you like to sell?" << endl;
-                            cin >> name1;
-                            for(int i = 0; i<p.size(); i++){
-                                if(p[i].getName()==name1){
-                                    int number = p[i].getHouses();
-                                    for(int j = 0; j<number; j++){
-                                        pl1.removeHouse(p[i].getName());
-                                        pl1.addMoney(p[i].getHouseCost());
+                            string name2;
+                            cout << pl1.getPlayerName() << " currently has places, " << names2 << "which would you like to sell?" << endl;
+                            cin >> name2;
+                            for(int i = 0; i<p1.size(); i++){
+                                if(p1[i].getName()==name2){
+                                    int number1 = p1[i].getHouses();
+                                    for(int j = 0; j<number1; j++){
+                                        pl1.removeHouse(p1[i].getName());
+                                        pl1.addMoney(p1[i].getHouseCost());
                                     }
-                                    pl1.removePlace(p[i].getName());
-                                    pl1.addMoney(p[i].getPrice());
+                                    pl1.removePlace(p1[i].getName());
+                                    pl1.addMoney(p1[i].getPrice());
                                 }
                             }
                             for(int i = 0; i<40; i++){
-                                if(places[i].getName()==name1){
-                                    int number = p[i].getHouses();
-                                    for(int j = 0; j<number; j++){
+                                if(places[i].getName()==name2){
+                                    int number1 = p1[i].getHouses();
+                                    for(int j = 0; j<number1; j++){
                                         places[i].removeHouse();
                                     }
                                     places[i].setOwned(false);
@@ -66,12 +69,18 @@ int num = 0;
                                 }
                             }
                         }
+                        break;
+                    }
                     case 3:
+                    {
                         cout << "Next turn" << endl;
                         break;
+                    }
                     default:
+                    {
                         cout << "Invalid input" << endl;
                         break;
+                    }
                 }
                 cout << "Current Balance: $" << pl1.getMoneyAmount() << endl;
                 vector<Place> p = pl1.listPlaces();
@@ -79,5 +88,5 @@ int num = 0;
                 for(int i = 0; i<p.size(); i++){
                     names += p[i].getName() + ", ";
                 }
-                cout << "You currently have, " << names << endl;
+                cout << pl1.getPlayerName() << " currently has, " << names << endl;
             }
